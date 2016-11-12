d1cc4a33b25fd06bca6af77477faa248fbd93a2f
John Hunter <john@pushagency.io>
2016-11-12 08:13:11 -0800

    added hibernate config

--- src/main/java/tech/eats/art/blog/config/DataConfig.java
+++ src/main/java/tech/eats/art/blog/config/DataConfig.java
@@ -1,7 +1,21 @@
 package tech.eats.art.blog.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
 /**
  * Created by John on 11/12/16.
  */
+@Configuration
 public class DataConfig {
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean() {
+        Resource config = new ClassPathResource("hibernate.cfg.xml");
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setConfigLocation(config);
+        return sessionFactoryBean;
+    }
 }
