5eff99ebb61d4303a9695b8db31988410596e47f
John Hunter <john@pushagency.io>
2016-11-12 07:52:16 -0800

    added appconfig

---
+++ src/main/java/tech/eats/art/blog/AppConfig.java
@@ -0,0 +1,15 @@
+package tech.eats.art.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by John on 11/12/16.
+ */
+@EnableAutoConfiguration
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+
+    }
+}
