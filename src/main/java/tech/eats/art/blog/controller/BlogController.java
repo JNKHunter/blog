608db8e8980b90e9d8186d5391c6f8d906e6a291
John Hunter <john@pushagency.io>
2016-11-15 15:07:53 -0800

    added blogindex

--- src/main/java/tech/eats/art/blog/controller/BlogController.java
+++ src/main/java/tech/eats/art/blog/controller/BlogController.java
@@ -1,10 +1,17 @@
 package tech.eats.art.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by John on 11/15/16.
  */
 @Controller
 public class BlogController {
+
+    @RequestMapping("/")
+    public String listBlog(Model model){
+        return "blog/index";
+    }
 }
