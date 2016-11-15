ee3e7bcc2c515ca635807980fd8e80c1d1df3c7d
John Hunter <john@pushagency.io>
2016-11-15 14:54:07 -0800

    added blogcontroller

---
+++ src/main/java/tech/eats/art/blog/controller/BlogController.java
@@ -0,0 +1,10 @@
+package tech.eats.art.blog.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by John on 11/15/16.
+ */
+@Controller
+public class BlogController {
+}
