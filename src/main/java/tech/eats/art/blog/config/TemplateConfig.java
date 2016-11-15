1c9084ec663ddf7e0e84ee8cd837a57d5cb58f1b
John Hunter <john@pushagency.io>
2016-11-15 15:01:06 -0800

    added template config

--- src/main/java/tech/eats/art/blog/config/TemplateConfig.java
+++ src/main/java/tech/eats/art/blog/config/TemplateConfig.java
@@ -1,7 +1,37 @@
 package tech.eats.art.blog.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
 /**
  * Created by John on 11/15/16.
  */
+@Configuration
 public class TemplateConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCacheable(false);
+        templateResolver.setPrefix("classpath:/templates/");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        final SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.addTemplateResolver(templateResolver());
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        final ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
 }
