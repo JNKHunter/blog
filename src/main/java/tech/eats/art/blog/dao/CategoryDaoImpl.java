109218aa47d1252522447d6b9f5fdd0da39c1d8b
John Hunter <john@pushagency.io>
2016-11-17 13:26:36 -0800

    implemented impl

--- src/main/java/tech/eats/art/blog/dao/CategoryDaoImpl.java
+++ src/main/java/tech/eats/art/blog/dao/CategoryDaoImpl.java
@@ -1,7 +1,27 @@
 package tech.eats.art.blog.dao;
 
+import tech.eats.art.blog.model.Category;
+
+import java.util.List;
+
 /**
  * Created by John on 11/17/16.
  */
 public class CategoryDaoImpl implements CategoryDao {
+
+    public List<Category> findAll() {
+        return null;
+    }
+
+    public Category findById(Long id) {
+        return null;
+    }
+
+    public void save(Category category) {
+
+    }
+
+    public void delete(Category category) {
+
+    }
 }
