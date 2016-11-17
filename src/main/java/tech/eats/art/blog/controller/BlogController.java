64552b7391cbbb164a9ed12af657fa59282cfd3e
John Hunter <john@pushagency.io>
2016-11-16 18:37:20 -0800

    format

--- src/main/java/tech/eats/art/blog/controller/BlogController.java
+++ src/main/java/tech/eats/art/blog/controller/BlogController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tech.eats.art.blog.model.Category;
+
+import java.util.List;
 
 /**
  * Created by John on 11/15/16.
@@ -13,17 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
 
-    //@Autowired
-    //private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @RequestMapping("/")
-    public String listBlog(Model model){
+    public String listBlog(Model model) {
         return "blog/index";
     }
 
+    @SuppressWarnings("unchecked")
     @RequestMapping("/categories")
-    public String listCategories(Model model){
-        //Session session = sessionFactory.openSession();
+    public String listCategories(Model model) {
+        Session session = sessionFactory.openSession();
+        List<Category> categories = session.createCriteria(Category.class).list();
+        model.addAttribute("categories", categories);
         return "blog/categories";
     }
 }
