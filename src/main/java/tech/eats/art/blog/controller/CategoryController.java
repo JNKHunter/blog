ca29e4dde9b61ae90959f9ca25e1d5e3789add46
John Hunter <john@pushagency.io>
2016-11-18 14:32:24 -0800

    moved category list controller out of blog controller

--- src/main/java/tech/eats/art/blog/controller/CategoryController.java
+++ src/main/java/tech/eats/art/blog/controller/CategoryController.java
@@ -1,7 +1,5 @@
 package tech.eats.art.blog.controller;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +10,23 @@ import tech.eats.art.blog.service.CategoryService;
 import java.util.List;
 
 /**
- * Created by John on 11/15/16.
+ * Created by John on 11/18/16.
  */
 @Controller
-public class BlogController {
+public class CategoryController {
 
     @Autowired
-    private CategoryService categoryService;
+    CategoryService categoryService;
 
-    @RequestMapping("/")
-    public String listBlog(Model model) {
-        return "blog/index";
+    @SuppressWarnings("unchecked")
+    @RequestMapping("/categories")
+    public String listCategories(Model model) {
+
+        List<Category> categories =  categoryService.findAll();
+
+        model.addAttribute("categories", categories);
+        return "blog/categories";
     }
+
+
 }
